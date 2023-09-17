a8dafef6727a2f2a43d334d49cf95bedc8d7c9f0
Dražen Buljovčić <drazen.buljovcic@gmail.com>
2023-09-17 11:01:39 +0200

    fix: version bash

--- deployment/rc-version.bash
+++ deployment/rc-version.bash
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 # Capture the output of the node command in a variable
-version=$(node -p "require('./package.json').version") 
+version=$(node -p "require('./package.json').version")
+
+commit=$(git log -1 --pretty=%H) 
 
 # Use the captured version as an argument for npm version
-npm version "$version"
+npm version "$version-rc.$$commit"
+
